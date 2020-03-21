5a3d6096915c9a86369abcb891e9d899ca35742a
lekimhongkhanh <lekimhongkhanh@gmail.com>
2020-03-21 12:11:25 +0700

    add button to sort movies by popularity

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/NavBar.js'
@@ -7,24 +7,49 @@ import MoviesList from './components/MoviesList.js'
 let apiKey = process.env.REACT_APP_APIKEY;
 
 function App() {
-  let [movies,setMovies] = useState ([]);
-  let [filterText,setFilterText] = useState ('');
+  let [movies, setMovies] = useState([]);
+  let [filterText, setFilterText] = useState('');
+
+  // let genres = async () => {
+  //   let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
+  //   let data = await fetch(url);
+  //   let dataResults = await data.json();
+  //   console.log("genres", dataResults);
+  //   // setMovies(dataResults.results)
+  // }
+
+  let sortByPopularity = () => {
+    const sortedMovies = [...movies]; // spread operator
+    // console.log('sort',movies.sort((a,b)=> b.popularity -a.popularity))
+    sortedMovies.sort((a,b)=> b.popularity -a.popularity);
+    console.log('sortedMovies after', sortedMovies);
+    setMovies(sortedMovies);
+  }
   
+
   let nowPlayingMovie = async () => {
     let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`
     let data = await fetch(url);
     let dataResults = await data.json();
-    console.log("data",dataResults);
+    console.log("data", dataResults);
     setMovies(dataResults.results)
   }
-  useEffect (nowPlayingMovie,[]);
+  useEffect(nowPlayingMovie, []);
   return (
     <div>
-    <NavBar setFilterText={setFilterText} />
-    < MoviesList moviesList = {movies.filter((movie)=>movie.title.includes(filterText))} />
-  <div> test: {filterText} </div>
+    <div className="navBar">
+
+      <NavBar 
+        setFilterText = {setFilterText} 
+        sortByPopularity={sortByPopularity}
+       />
+      </div>
+      <div className="moviesContainer">
+      < MoviesList moviesList={movies.filter((movie) => movie.title.toLowerCase().includes(filterText))} />
+      
     </div>
-    
+    </div>
+
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import MoviesList from './MoviesList';
 
-function NavBar (props) {
-  const [keyword,setKeyword] = useState ('');
-    return (
-        <div>
-<Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={(e)=>{setKeyword(e.target.value); console.log("key",keyword)}} />
-      <Button onClick={()=> props.setFilterText(keyword)} variant="outline-success">Search</Button>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>
-</div>
-    )
+function NavBar(props) {
+  const [keyword, setKeyword] = useState('');
+  
+  return (
+    <div className="navBar">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Khanh's Movies</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Animation</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Comody</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">All Movies</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={(e) => { setKeyword(e.target.value) }} />
+            <Button onClick={() => props.setFilterText(keyword)} variant="outline-success">Search</Button>
+            <Button onClick={()=>props.sortByPopularity()} variant="outline-success"> The Most Popular </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  )
 }
 
 export default NavBar;
